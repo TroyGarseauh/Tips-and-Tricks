dd5bee0607b3d267ee4bcc7a37ce28318e8c35da
Troy Garcia <108847138+TroyGarseauh@users.noreply.github.com>
2022-08-05 05:17:17 +0000

    Created GenericReactComponent
    This is a TypeScript Tip for a React Component using Generics. August 4th 2022

---
+++ TypeScript-Tips/GenericReactComponent.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+/*
+
+Making a Table Component that uses TypeScript generics to render any
+data passed to it.
+
+Twitter and GitHub @troygarseauh
+
+Please roast my code / provide examples of how you'd solve 
+or explain this better.
+
+Let's grow together!
+
+*/
+
+
+
+interface TableData<TData> {
+    data:TData[]; 
+    renderedData:(item:TData)=>React.ReactNode;
+}
+
+
+export const Table = <TData,>(props:TableData<TData>)=>{
+    return null;
+};
+
+export const TableComponent = () => {
+  return (
+   <>
+    <Table
+    data={[{id:"1", productName:"Gouda Cheese"}]}
+    renderedData={(data) => {
+        return null;
+    }}
+    ></Table>
+
+    <Table
+    data={[{id:"1", expensiveProduct:"Fancy Wine"}]}
+    renderedData={(data) => {
+        return null;
+    }}
+    ></Table>
+   
+   </>
+  )
+}
